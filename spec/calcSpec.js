7163ab265b9b576f7ec539fa9a6690e2c81d1458
Rang3rDang3r <stu-evans@live.co.uk>
2019-11-20 12:02:25 +0000

    added 'spyOn' function for alert box

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -6,8 +6,10 @@ describe ("Calculator", function(){
         it("should return 26", function(){
             expect(addition(7, 19)) .toBe(26);
         })
-        it("should return an error if no numbers are used", function(){
-            expect(addition("hitchhikers", "guide")) .toBe("Error");
+        it("should return an error if we dont supply two numbers", function(){
+            spyOn (window, "alert");
+            addition("hithikers", "guide");
+            expect(window.alert) .toHaveBeenCalledWith("Error");
         })
     })
 })
